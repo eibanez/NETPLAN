677f3e7bc5c30b427a847a337b907fa3be0082cd
Eduardo Ibanez <edu.ibanez@gmail.com>
2011-09-22 00:07:57 -0500

    Convert Index to structure

--- src/index.h
+++ src/index.h
@@ -8,23 +8,22 @@
 #define _INDEX_H_
 
 // Declare class type to hold index information
-class Index {
-	public:
-		Index();
-		Index(const Index& rhs);
-		~Index();
-		Index& operator=(const Index& rhs);
-		
-		void Add(const int newpos, const int newcol, const int newyear, const string& newname);
-		void Add(const int newpos, const Step& newcol, const string& newname);
-		void WriteFile(const char* fileinput) const;
-		
-		// Variables
-		int start, size;
-		vector<int> position;
-		vector<int> column;
-		vector<int> year;
-		vector<string> name;
+struct Index {
+	Index();
+	Index(const Index& rhs);
+	~Index();
+	Index& operator=(const Index& rhs);
+	
+	void Add(const int newpos, const int newcol, const int newyear, const string& newname);
+	void Add(const int newpos, const Step& newcol, const string& newname);
+	void WriteFile(const char* fileinput) const;
+	
+	// Variables
+	int start, size;
+	vector<int> position;
+	vector<int> column;
+	vector<int> year;
+	vector<string> name;
 };
 
 Index ReadFile(const char* fileinput);
